989c3a10d61e00f333487797427d539b32e71dbe
marleenkj <marleen-j@web.de>
2023-09-12 08:05:25 +0200

    app clean

--- ui/app/app.py
+++ ui/app/app.py
@@ -3,8 +3,6 @@ import dash
 import dash_bootstrap_components as dbc
 #import dash_auth
 import pandas as pd
-import datetime as dt
-import numpy as np
 from ast import literal_eval
 import json
 
@@ -12,33 +10,29 @@ route_preffix = os.environ.get('EXTRAPATH', '') + '/'
 
 app_name = "Decision Support Tool for CRFT"
 
-"""
-# Import data
-project_path = '../../data'
-dataset_name = 'poc3'
-
-df = pd.read_csv(f'{project_path}/processed/{dataset_name}.csv', 
-                 low_memory=False,
-                 parse_dates=['Pickup date'], 
-                 dtype={'DC zip': str, 'Zip': str}).sort_values(by='Pickup date')
-df = df.dropna(subset = ['Distance (km)'])
-"""
-
 # Import data
 project_path = '../../notebooks/results'
 dataset_name = 'df_limited'
-df_limited = pd.read_csv(f'{project_path}/{dataset_name}.csv', parse_dates=['Delivery date'])
+df_limited = pd.read_csv(
+    f'{project_path}/{dataset_name}.csv',
+    parse_dates=['Delivery date'])
 dataset_name = 'df_distance_matrix'
-df_distance_matrix = pd.read_csv(f'{project_path}/{dataset_name}.csv', index_col = 0)
-df_results_details = pd.read_csv(f'{project_path}/df_results_details.csv', index_col = [0])
+df_distance_matrix = pd.read_csv(
+    f'{project_path}/{dataset_name}.csv',
+    index_col=0)
+df_results_details = pd.read_csv(
+    f'{project_path}/df_results_details.csv',
+    index_col=[0])
 dataset_name = 'df_results_direct_ftl_truck_speed_80_80'
-results_direct_ftl_truck = pd.read_csv(f'{project_path}/direct/{dataset_name}.csv')
+results_direct_ftl_truck = pd.read_csv(
+    f'{project_path}/direct/{dataset_name}.csv')
 dataset_name = 'df_results_direct_ftl_train_speed_80_80'
-results_direct_ftl_train = pd.read_csv(f'{project_path}/direct/{dataset_name}.csv')
+results_direct_ftl_train = pd.read_csv(
+    f'{project_path}/direct/{dataset_name}.csv')
 dataset_name = '0730_results_base_weekly'
 df_ghg = pd.read_csv(f'{project_path}/old/{dataset_name}.csv')
 df_ghg_converted = pd.read_csv(
-    f'{project_path}/old/{dataset_name}.csv', 
+    f'{project_path}/old/{dataset_name}.csv',
     converters={'routes railroad': literal_eval, 'routes road': literal_eval})
 
 # Import dict terminals
@@ -50,33 +44,33 @@ f = open("../../data/processed/dict_points.json")
 dict_points = json.load(f)
 
 dict_results = {
- 'co2 road': 2008.889022706869,
- 'distance road': 2477629,
- 'routes road': [['DC2', 'C22', 'C13', 'C27', 'C15', 'C28', 'C24', 'DC2']],
- 'processing time road': 0.890625,
- 'co2 railroad': 1457.7977660290946,
- 'distance railroad': 3439485.0,
- 'terminal allocation': [[],
-  ['C24', 'C28'],
-  [],
-  ['C13', 'C27'],
-  ['C15'],
-  [],
-  ['C22'],
-  [],
-  [],
-  []],
- 'routes railroad': {'T1': [[]],
-  'T2': [['T2', 'C28', 'C24', 'T2']],
-  'T3': [[]],
-  'T4': [['T4', 'C27', 'C13', 'T4']],
-  'T5': [['T5', 'C15', 'T5']],
-  'T6': [[]],
-  'T7': [['T7', 'C22', 'T7']],
-  'T8': [[]],
-  'T9': [[]],
-  'T10': [[]]},
- 'processing time railroad': 3.734375}
+    'co2 road': 2008.889022706869,
+    'distance road': 2477629,
+    'routes road': [['DC2', 'C22', 'C13', 'C27', 'C15', 'C28', 'C24', 'DC2']],
+    'processing time road': 0.890625,
+    'co2 railroad': 1457.7977660290946,
+    'distance railroad': 3439485.0,
+    'terminal allocation': [[],
+                            ['C24', 'C28'],
+                            [],
+                            ['C13', 'C27'],
+                            ['C15'],
+                            [],
+                            ['C22'],
+                            [],
+                            [],
+                            []],
+    'routes railroad': {'T1': [[]],
+                        'T2': [['T2', 'C28', 'C24', 'T2']],
+                        'T3': [[]],
+                        'T4': [['T4', 'C27', 'C13', 'T4']],
+                        'T5': [['T5', 'C15', 'T5']],
+                        'T6': [[]],
+                        'T7': [['T7', 'C22', 'T7']],
+                        'T8': [[]],
+                        'T9': [[]],
+                        'T10': [[]]},
+    'processing time railroad': 3.734375}
 
 app = dash.Dash(
     __name__,
--- ui/app/index.py
+++ ui/app/index.py
@@ -1,9 +1,7 @@
 import base64
-
 from dash.dependencies import Input, Output
 from dash import dcc, html
 import dash_bootstrap_components as dbc
-
 from app import app
 
 from pages import page_list
@@ -25,26 +23,35 @@ TOPBAR_STYLE = {
 NAVLINK_STYLE = {
     "margin-left": "2rem",
     "color": "#F0182D",
-    #"font-size": 18,
-    #"font-weight": 900,
+    # "font-size": 18,
+    # "font-weight": 900,
 }
 
 encoded_logo = base64.b64encode(open('assets/Logo_TU.png', 'rb').read())
-logo = html.Img(src='data:image/jpg;base64,{}'.format(encoded_logo.decode()), height='30px')
+logo = html.Img(
+    src='data:image/jpg;base64,{}'.format(encoded_logo.decode()), height='30px')
 branding = dbc.Row(
     [dbc.Col(e) for e in [logo, dbc.NavbarBrand(app_name, className="ms-2")]],
     align="center",
     className="g-0",
 )
 
-pages = [dbc.NavLink(page.name, href=page.path, id=page.id_, style=NAVLINK_STYLE) for page in page_list]
-navbar = dbc.Navbar(
-    [html.A(branding, href=home.path, id=home.id_, style={"textDecoration": "none"}), ] + pages,
-    color="dark",
-    dark=True,
-    style=TOPBAR_STYLE,
-    class_name='ml-auto'
-)
+pages = [
+    dbc.NavLink(
+        page.name,
+        href=page.path,
+        id=page.id_,
+        style=NAVLINK_STYLE) for page in page_list]
+navbar = dbc.Navbar([html.A(branding,
+                            href=home.path,
+                            id=home.id_,
+                            style={"textDecoration": "none"}),
+                     ] + pages,
+                    color="dark",
+                    dark=True,
+                    style=TOPBAR_STYLE,
+                    class_name='ml-auto')
+
 
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
@@ -53,10 +60,9 @@ def display_page(pathname):
             return page.layout
     return home.layout
 
-app.layout = html.Div(
-    [dcc.Location(id="url"), navbar, html.Div(id="page-content", style=CONTENT_STYLE)],
-)
+
+app.layout = html.Div([dcc.Location(id="url"), navbar, html.Div(
+    id="page-content", style=CONTENT_STYLE)], )
 
 if __name__ == "__main__":
     app.run_server(port=5000, debug=True)
-
